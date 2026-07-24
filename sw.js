53017a7260276a8647745a10a0c575a29c332e11
duchluu1-sys <duc.hluu1@gmail.com>
2026-07-24 12:31:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("install", event => {
+    event.waitUntil(self.skipWaiting());
+});
 
-self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+});
 
 self.addEventListener("fetch", event => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
